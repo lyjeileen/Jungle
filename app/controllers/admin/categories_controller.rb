b27ac73301049d8f2048a7f174292bdba35a7c47
lyjeileen <lyjeileen@gmail.com>
2022-11-17 23:42:28 -0800

    Add index, new, create actions

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,27 @@
 class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @categories = Category.new
+  end
+
+  def create
+    @categories = Category.new(categories_params)
+
+    if @product.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:categories).permit(
+      :name
+    )
+  end
 end
