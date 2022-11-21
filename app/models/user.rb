b4d2ea22edcbcb90b0cccb0db724e42b881d5b30
lyjeileen <lyjeileen@gmail.com>
2022-11-21 15:42:32 -0800

    Add more validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
+  validates :email, presence: true, uniqueness: true, case_sensitive: false, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
