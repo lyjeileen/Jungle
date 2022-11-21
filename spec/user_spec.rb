9049f081c42cda168d2178fd8e98a3e5a9461278
lyjeileen <lyjeileen@gmail.com>
2022-11-21 15:42:21 -0800

    Add validation tests

---
+++ spec/user_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject {
+    User.new(first_name: "First",
+      last_name: "Last",
+      email: "test@test.com",
+      password: "123",
+      password_confirmation: "123")
+  }
+  describe 'Validations' do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is invalid if password and password_confirmation don't match" do
+      subject.password="12"
+      expect(subject).to_not be_valid
+    end
+
+    it "is invalid without an email" do
+      subject.email=nil
+      expect(subject).to_not be_valid
+    end
+   
+    it "is invalid without user's first name" do
+      subject.first_name=nil
+      expect(subject).to_not be_valid
+    end
+    it "is invalid without user's last name" do
+      subject.last_name=nil
+      expect(subject).to_not be_valid
+    end
+
+  end
+end
