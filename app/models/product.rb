55c2881c168a93aaa04867536e1df39462846f5b
lyjeileen <lyjeileen@gmail.com>
2022-11-22 14:11:11 -0800

    Add class method to check if a product is sold out

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,8 @@ class Product < ApplicationRecord
   validates :price, presence: true
   validates :quantity, presence: true
   validates :category, presence: true
+
+  def self.soldout?(quantity)
+    quantity==0
+  end
 end
