86c7fe446ac2883d0b43b25944c4c44cbf7718c3
lyjeileen <lyjeileen@gmail.com>
2022-11-21 18:01:33 -0800

    Solve edge cases
    Add case insensitive setting; remove empty space for email input, convert all the email input to lowercase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password, length: { minimum: 4 }
+
   def self.authenticate_with_credentials (email,password)
-    user = User.find_by_email(email)
+    user = User.find_by_email(email.strip.downcase)
+    
     if user && user.authenticate(password)
       user
     else
