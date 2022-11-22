1337925918c9c6ffc60b7e8e7ebc1d3b3aa0fc98
lyjeileen <lyjeileen@gmail.com>
2022-11-21 16:04:02 -0800

    Add password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, presence: true, uniqueness: true, case_sensitive: false, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :password, length: { minimum: 4 }
 end
