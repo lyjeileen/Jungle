23e0bb3dd247a7a94872aaf4cc3d5799dad7a200
lyjeileen <lyjeileen@gmail.com>
2022-11-21 23:23:01 -0800

    Add one more test to check the number of products

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,7 +2,12 @@ describe('Testing Home page', () => {
   beforeEach(() => {
     cy.visit('/');
   });
+
   it('There is products on the page', () => {
     cy.get('.products article').should('be.visible');
+  });
+
+  it('There is 2 products on the page', () => {
+    cy.get('.products article').should('have.length', 2);
   });
 });
